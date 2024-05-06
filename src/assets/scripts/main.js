67f036b701c1671197629c954883239781e865c3
Jose Mazuelos <pepemazuelos@gmail.com>
2024-05-06 10:51:24 +0200

    first commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,11 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+//import "./header/_header-nav.js";
+
+/**
+ * Import Bootstrap’s JS.
+ */
+import * as bootstrap from "bootstrap";
